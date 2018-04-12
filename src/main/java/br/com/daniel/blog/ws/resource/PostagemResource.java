4dfbeef908d032946340b8daad85bdc3476df881
DanielSilverio <daniel.silverio@metasix.com.br>
2018-04-12 10:11:07 -0300

    Altera PostagemResource para consumir PostagemService

--- src/main/java/br/com/daniel/blog/ws/resource/PostagemResource.java
+++ src/main/java/br/com/daniel/blog/ws/resource/PostagemResource.java
@@ -1,27 +1,30 @@
 package br.com.daniel.blog.ws.resource;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.daniel.blog.ws.entity.Postagem;
+import br.com.daniel.blog.ws.service.impl.PostagemServiceImpl;
 
 @RestController
 @RequestMapping("postagens")
 public class PostagemResource {
+	
+	@Autowired
+	private PostagemServiceImpl postagemService;
 
 	@RequestMapping( method= RequestMethod.POST )
 	public void save(Postagem postagem) {
-		// save
+		postagemService.save(postagem);
 	}
 	
 	@RequestMapping( method = RequestMethod.GET )
 	public List<Postagem> findAll(){
-		List<Postagem> postagens = new ArrayList<>();
-		return postagens;
+		return postagemService.findAll();
 	}
 	
 }
