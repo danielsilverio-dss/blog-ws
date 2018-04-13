e5d0de855a1a4375321f5694e8b30b91aacb3671
DanielSilverio <daniel.silverio@metasix.com.br>
2018-04-13 09:27:52 -0300

    Corrige configuração de endpoint em PostagemResource

--- src/main/java/br/com/daniel/blog/ws/resource/PostagemResource.java
+++ src/main/java/br/com/daniel/blog/ws/resource/PostagemResource.java
@@ -3,6 +3,8 @@ package br.com.daniel.blog.ws.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,9 @@ public class PostagemResource {
 	@Autowired
 	private PostagemService postagemService;
 
-	@RequestMapping( method= RequestMethod.POST )
-	public void save(Postagem postagem) {
+	@RequestMapping( method = RequestMethod.POST,
+					 consumes = MediaType.APPLICATION_JSON_VALUE )
+	public void save(@RequestBody Postagem postagem) {
 		postagemService.save(postagem);
 	}
 	
